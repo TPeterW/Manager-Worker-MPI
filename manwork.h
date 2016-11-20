3df01b73c7defe391b23229dcd09ae48e417df8d
Tao Peter Wang <TPeterW@users.noreply.github.com>
2016-11-20 13:49:27 -0500

    Update manwork.h

--- manwork.h
+++ manwork.h
@@ -13,23 +13,3 @@ void generator(void *payload, lqueue_t *qjobs);
 void process(void *payload, void *result);
 
 void display(lqueue_t *qresult, int np);
-
-
-// #define index(x, y) (x) * size + (y)
-// struct processPackage {
-//     uint8_t board[100];     // for now supports maximum 10 x 10
-//     int size;
-//     int depth;
-// };
-// void
-// printB(uint8_t *board, int size)
-// {
-//     int i, j;
-//     for (i = 0; i < size; i++) {
-//         for (j = 0; j < size; j++) {
-//             printf("%c", board[index(i, j)] ? 'Q' : '-');
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-// }
